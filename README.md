0aa2641c2d97bb438c80336dd9220da3295ab824
brunau1 <brunosantosprotec@gmail.com>
2022-08-10 02:15:10 -0300

    feat: game assets adjustment

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mountain-racing
 Mountain racing crazy game
+
+jogo pega-pega 
+ao encostar nas bordas recebe impulso na direção contrária
+impulso mais fraco em quem foge, mas quem foge é mais lento
--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
-const SCREEN_WIDTH = 1280;
-const SCREEN_HEIGHT = 720;
+const SCREEN_WIDTH = 1080;
+const SCREEN_HEIGHT = 760;
 const WORLD_BG = "./assets/scenes/world_bg.png";
 const PLAYER_IMG = "./assets/personas/persona_1.png";
+const PLAYER_WIDTH = 140;
+const PLAYER_HEIGHT = 110;
 const MOUNTAIN_IMG = "./assets/scenes/mountains_side.png";
 const STREET_IMG = "./assets/scenes/street_side.png";
-
 class GameEngine {
   constructor() {
     this.canvas = document.createElement("canvas");
@@ -51,24 +52,42 @@ class ScrollingComponent {
   }
 
   drawImageRepeat(context) {
-    context.drawImage(this.image, this.x, this.y);
-    context.drawImage(this.image, this.x + this.width, this.y);
-    context.drawImage(this.image, this.x - this.width, this.y);
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    context.drawImage(
+      this.image,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    );
+    context.drawImage(
+      this.image,
+      this.x - this.width,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+
+  update(context) {
+    this.scroll(context);
   }
 }
-class GameComponent {
+class Player {
   constructor(imagePath, x, y, accX, accY, width, height) {
     this.x = x;
     this.y = y;
 
-    this.speedX = 0;
-    this.speedY = 0;
+    this.speedX = 0.2;
+    this.speedY = 0.2;
 
     this.accX = accX;
     this.accY = accY;
 
     this.width = width;
     this.height = height;
+
+    this.movingState = "stop";
 
     this.image = new Image();
     this.image.src = imagePath;
@@ -82,13 +101,13 @@ class GameComponent {
   move(direction) {
     const actions = {
       up: () => {
-        this.speedY -= this.accY;
+        this.speedY += -this.accY;
       },
       down: () => {
         this.speedY += this.accY;
       },
       left: () => {
-        this.speedX -= this.accX;
+        this.speedX += -this.accX;
       },
       right: () => {
         this.speedX += this.accX;
@@ -106,27 +125,81 @@ class GameComponent {
   draw(context) {
     context.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
-}
 
-const gamePlayers = [];
-const gameScenario = [];
+  update(context) {
+    this.randomDirection();
+    this.move(this.movingState);
+    this.checkWorldCollision();
+    this.draw(context);
+  }
+
+  checkWorldCollision() {
+    // left
+    if (this.x < 0) {
+      this.x = 0;
+      // right
+    } else if (this.x + this.width > SCREEN_WIDTH) {
+      this.x = SCREEN_WIDTH - this.width;
+    }
+    // top
+    if (this.y < 0) {
+      this.y = 0;
+      // bottom
+    } else if (this.y + this.height > SCREEN_HEIGHT) {
+      this.y = SCREEN_HEIGHT - this.height;
+    }
+  }
+
+  randomDirection() {
+    const directions = ["up", "down", "left", "right", "stop"];
+    if (count % 10 === 0)
+      this.movingState = directions[Math.floor(Math.random() * directions.length)];
+    count++;
+  }
+}
+let count = 0;
+
+const gameComponents = [];
 const GAME_RUNTIME = new GameEngine();
 
 function startGame() {
+  const mountain_side = new ScrollingComponent(
+    MOUNTAIN_IMG,
+    0,
+    230,
+    SCREEN_WIDTH,
+    170,
+    1
+  );
+  const street_side = new ScrollingComponent(
+    STREET_IMG,
+    0,
+    400,
+    SCREEN_WIDTH,
+    360,
+    4
+  );
+  const player1 = new Player(
+    PLAYER_IMG,
+    30,
+    400,
+    0.2,
+    0.2,
+    PLAYER_WIDTH,
+    PLAYER_HEIGHT
+  );
+
   GAME_RUNTIME.start();
-  gamePlayers.push(new GameComponent(PLAYER_IMG, 30, 400, 1, 1, 160, 120));
-  gameScenario.push(new ScrollingComponent(MOUNTAIN_IMG, 0, 230, 1280, 170, 1));
-  gameScenario.push(new ScrollingComponent(STREET_IMG, 0, 400, 1280, 350, 4));
+  gameComponents.push(...[mountain_side, street_side, player1]);
   updateGame();
 }
 
 function updateGame() {
-  window.requestAnimationFrame(updateGame);
-
   console.log("update");
   GAME_RUNTIME.clear();
-  for (const element of gameScenario) element.scroll(GAME_RUNTIME.context);
-  for (const element of gamePlayers) element.draw(GAME_RUNTIME.context);
+  for (const element of gameComponents) element.update(GAME_RUNTIME.context);
+
+  window.requestAnimationFrame(updateGame);
 }
 
 startGame();
